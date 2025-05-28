d788d045d99a3830ee52933ae0ff4e2f8a14f004
usertree12 <Maorm22@outlook.com>
2025-05-28 16:41:14 +0300

    Update

--- Server/obj/Debug/net8.0/Server.AssemblyInfo.cs
+++ Server/obj/Debug/net8.0/Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3acad6c1a72b72b2ba343cd93950948b2fe20e91")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
